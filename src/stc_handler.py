adf952c7c9d858a75b92cbaaf2b16acb375b727a
LuizaNacshon <luiza.n@quali.com>
2017-01-30 16:37:25 +0200

    json and csv format fix

--- src/stc_handler.py
+++ src/stc_handler.py
@@ -12,6 +12,7 @@ import json
 import csv
 import io
 from testcenter.stc_statistics_view import StcStats
+import os
 
 
 class StcHandler(object):
@@ -20,11 +21,13 @@ class StcHandler(object):
         """
         :type context: cloudshell.shell.core.driver_context.InitCommandContext
         """
-
+        curr_dir = os.getcwd()
+        log_dir = curr_dir+'/Logs'
+        log_file = 'STC_logger.log'
         client_install_path = context.resource.attributes['Client Install Path']
-        logging.basicConfig(filename='example.log', level=logging.DEBUG)
+        logging.basicConfig(filename= log_file, level=logging.DEBUG)
         self.logger = logging.getLogger('root')
-        self.logger.addHandler(logging.FileHandler('example.log'))
+        self.logger.addHandler(logging.FileHandler(log_file))
         self.logger.setLevel('DEBUG')
 
         self.tcl_interp = TgnTkMultithread()
@@ -103,12 +106,12 @@ class StcHandler(object):
                     physical_add = re.sub(r'[^./0-9 ]', r'', FullAddress)
                     self.logger.info("Logical Port %s will be reserved now on Physical location %s" %
                                      (str(port_name), str(physical_add)))
-                    port.reserve(physical_add)
+                    port.reserve(physical_add,force=True)
 
         else:
             for port_name, port in self.ports.items():
                 # 'physical location in the form ip/module/port'
-                port.reserve()
+                port.reserve(force=True)
 
         self.logger.info("Port Reservation Completed")
 
@@ -146,6 +149,12 @@ class StcHandler(object):
 
         self.stc.stop_traffic()
 
+    def _csv2string(self,data):
+        si = io.BytesIO()
+        cw = csv.writer(si)
+        cw.writerow(data)
+        return si.getvalue().strip('\r\n')
+
     def get_statistics(self, context, view_name, output_type):
         output_file = output_type.lower().strip()
         if output_file != 'json' and output_file != 'csv':
@@ -156,12 +165,13 @@ class StcHandler(object):
         reservation_id = context.reservation.reservation_id
         my_api = self.get_api(context)
         if output_file.lower() == 'json':
-            statistics = json.dumps(statistics, ensure_ascii=False)
+            statistics = json.dumps(statistics, indent=4, sort_keys=True,ensure_ascii=False)
             # print statistics
-            my_api.WriteMessageToReservationOutput(reservation_id, str(statistics))
+            my_api.WriteMessageToReservationOutput(reservation_id, statistics)
         elif output_file.lower() == 'csv':
             output = io.BytesIO()
             w = csv.DictWriter(output, statistics.keys())
             w.writeheader()
             w.writerow(statistics)
-            my_api.WriteMessageToReservationOutput(reservation_id, str(output.getvalue()))
+
+            my_api.WriteMessageToReservationOutput(reservation_id,output.getvalue().strip('\r\n'))
