44bdba4e29976867cf1efff79b439406b5e6a1af
LuizaNacshon <luiza.n@quali.com>
2017-04-06 12:55:47 +0300

    uncommited changes

--- src/driver.py
+++ src/driver.py
@@ -9,6 +9,7 @@ from cloudshell.shell.core.context_utils import get_resource_name
 class TestCenterControllerDriver(ResourceDriverInterface):
 
     def __init__(self):
+        self.status = "keep_alive"
         self.handler = StcHandler()
 
 
@@ -32,6 +33,14 @@ class TestCenterControllerDriver(ResourceDriverInterface):
 
         return self.handler.get_inventory(context)
 
+
+    def _run_api(self,context,liveStatusName,additionalInfo):
+        my_api = self.handler.get_api(context)
+        resource_name = get_resource_name(context=context)
+        my_api.SetResourceLiveStatus(resourceFullName=resource_name, liveStatusName=liveStatusName, \
+                                     additionalInfo=additionalInfo)
+
+
     def load_config(self, context, stc_config_file_name, get_data_from_config):
         """ Load STC configuration file and reserve ports.
         :param context: the context the command runs on
@@ -45,8 +54,9 @@ class TestCenterControllerDriver(ResourceDriverInterface):
         reservation_id = context.reservation.reservation_id
         resource_name = get_resource_name(context=context)
         my_api.EnqueueCommand(reservationId=reservation_id,targetName=resource_name,commandName="keep_alive", targetType="Resource")
-
+        self._run_api(context,'Info','Running load config command')
         self.handler.load_config(context, stc_config_file_name, get_data_from_config)
+        self._run_api(context, 'Online', 'In keep alive state')
         return ""
 
     def send_arp(self, context):
@@ -54,8 +64,9 @@ class TestCenterControllerDriver(ResourceDriverInterface):
         :param context: the context the command runs on
         :type context: cloudshell.shell.core.driver_context.ResourceRemoteCommandContext
         """
-
+        self._run_api(context, 'Info', 'Send ARP')
         self.handler.send_arp(context)
+        self._run_api(context, 'Online', 'In keep alive state')
         return ""
 
     def start_devices(self, context):
@@ -63,8 +74,9 @@ class TestCenterControllerDriver(ResourceDriverInterface):
         :param context: the context the command runs on
         :type context: cloudshell.shell.core.driver_context.ResourceRemoteCommandContext
         """
-
+        self._run_api(context, 'Info', 'Start Devices')
         self.handler.start_devices(context)
+        self._run_api(context, 'Online', 'In keep alive state')
         return ""
 
     def stop_devices(self, context):
@@ -72,27 +84,32 @@ class TestCenterControllerDriver(ResourceDriverInterface):
         :param context: the context the command runs on
         :type context: cloudshell.shell.core.driver_context.ResourceRemoteCommandContext
         """
-
+        self._run_api(context, 'Info', 'Stop Devices')
         self.handler.stop_devices(context)
+        self._run_api(context, 'Online', 'In keep alive state')
 
     def start_traffic(self, context,blocking):
         """
         :param context: the context the command runs on
         :type context: cloudshell.shell.core.driver_context.ResourceRemoteCommandContext
         """
-
+        self._run_api(context, 'Info', 'Start Traffic')
         self.handler.start_traffic(context,blocking)
+        self._run_api(context, 'Online', 'In keep alive state')
 
     def stop_traffic(self, context):
         """
         :param context: the context the command runs on
         :type context: cloudshell.shell.core.driver_context.ResourceRemoteCommandContext
         """
-
+        self._run_api(context, 'Info', 'Stop Traffic')
         self.handler.stop_traffic(context)
+        self._run_api(context, 'Online', 'In keep alive state')
 
     def get_statistics(self, context, view_name, output_type):
+        self._run_api(context, 'Info', 'Get Statistics')
         self.handler.get_statistics(context, view_name, output_type)
+        self._run_api(context, 'Online', 'In keep alive state')
         return ""
 
     def cleanup(self):
